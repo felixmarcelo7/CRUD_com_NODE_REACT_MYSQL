b776088e76c3070673c7b4beb0d579b394a7dbdd
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2025-09-01 09:17:58 -0300

    formulário estilizado

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 
 const FormContainer = styled.form`
-    display: flex;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
 `;
 
-const Form = () => {
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+  text-align: left;
+  margin-top: 25px;
+`;
+
+const Form = ({ onEdit }) => {
+  const ref = useRef();
+
   return (
     <FormContainer>
-      <div>
+      <InputArea>
         <label htmlFor="nome">Nome</label>
-        <input type="text" name="nome" id="nome" />
-      </div>
-      <div>
+        <Input type="text" name="nome" id="nome" />
+      </InputArea>
+      <InputArea>
         <label htmlFor="email">E-mail</label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div>
+        <Input type="email" name="email" id="email" />
+      </InputArea>
+      <InputArea>
         <label htmlFor="tel">Telefone</label>
-        <input type="tel" name="tel" id="tel" />
-      </div>
-      <div>
+        <Input type="tel" name="tel" id="tel" />
+      </InputArea>
+      <InputArea>
         <label htmlFor="dataNasc">Data de Nascimento</label>
-        <input type="date" name="dataNasc" id="dataNasc" />
-      </div>
-      <input type="submit" value="SALVAR" />
+        <Input type="date" name="dataNasc" id="dataNasc" />
+      </InputArea>
+      <Button type="submit">SALVAR</Button>
     </FormContainer>
   );
 };
