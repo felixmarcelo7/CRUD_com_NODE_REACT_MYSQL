1da49a7ed7d6d7800ae8438169e44e4495a98f0b
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2025-09-04 10:57:40 -0300

    criada conexão com banco de dados e renderiza

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./components/Form.jsx";
 import GlobalStyle from "./styles/global";
+import Grid from "./components/Grid.jsx";
 import styled from "styled-components";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -17,13 +19,29 @@ const Container = styled.div`
 
 const Title = styled.h2``;
 
-
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, serOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
         <Form />
+        <Grid users={users}/>
       </Container>
       <ToastContainer />
       <GlobalStyle />
