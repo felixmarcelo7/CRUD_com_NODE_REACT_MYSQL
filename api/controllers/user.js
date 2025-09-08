d167895c8093b4c35c93acdeec511fed325cf884
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2025-09-08 10:32:38 -0300

    backend pronto

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,4 +9,47 @@ const getUsers = (_, res) => {
   });
 };
 
+export const addUsers = (req, res) => {
+  const q =
+    "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_nascimento,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+    return res.status(201).json("Usuário criado com sucesso!");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_nascimento,
+  ];
+
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Usuário atualizado com sucesso!");
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM usuarios WHERE `id` = ?";
+
+  db.query(q, req.params.id, (err) => {
+    if (err) return res.json(err);
+    return res.status(204).json("Usuário deletado com sucesso!");
+  });
+};
+
 export default getUsers;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,15 @@
 import express from "express";
-import getUsers from "../controllers/user.js";
+import getUsers, {
+  addUsers,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.post("/", addUsers);
+router.put("/", updateUser);
+router.delete("/", deleteUser);
 
 export default router;
